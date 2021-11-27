af1c73de49987a7a989849d82190783ec2592d69
ingramwd <Ingramwd01@gmail.com>
2021-11-27 17:41:27 -0500

    added buttons and css to homepage and added item array of questions

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+var questionsArr = [
+    {
+        Question: "Who created JavaScript?",
+        A: "Al Gore",
+        B: "Brendan Eich",
+        C: "Bill Gates",
+        D: "Steve Jobs",
+    },
+    {
+        Question: "What does CSS stand for?",
+        A: "cascading style sheets",
+        B: "creative style sheets",
+        C: "compact style script",
+        D: "compressed style sheets",
+    },
+    {
+        Question: "What allows users to interact with web pages?",
+        A: "HTML",
+        B: "CSS",
+        C: "Javascript",
+        D: "Mouse & Keyboards",
+    },
+    {
+        Question: "Can you add HTML elements dynamically with Javascript",
+        A: "No",
+        B: "Maybe",
+        C: "Only with jquery",
+        D: "Yes",
+    },
+];
+
+document.getElementById("start").addEventListener("click", console.log("Hello!"));
